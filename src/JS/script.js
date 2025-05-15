77bbb1a0355673c4851560bc3e82dbb0b5195337
Pedro Henrique <pedrogregolini@gmail.com>
2025-05-15 18:04:17 -0300

    Adicionando e aprendendo a usar Swiper

---
+++ src/JS/script.js
@@ -0,0 +1,12 @@
+<script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
+const swiper = new Swiper('.swiper-principal', {
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
